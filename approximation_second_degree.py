790909dc7ca9e8e96a2f4488b8007fc334dbd0ae
Polina Nesterova <89970688+polmustdie@users.noreply.github.com>
2022-12-16 16:37:19 +0300

    Create approximation_second_degree.py

---
+++ approximation_second_degree.py
@@ -0,0 +1,52 @@
+import approximation as aprx
+import numpy as np
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    NumberGroup = 10
+    NumberStudent = 13
+    a = NumberGroup + NumberStudent
+    listX = [(i + 0.5) * a for i in range(-3, 2, 1)]
+    listY = [84 - NumberGroup, 73 - NumberGroup * 2, 63 - NumberGroup * 3, 55 - NumberGroup * 4, 47 - NumberGroup * 5]
+
+    print('X = ', listX)
+    print('Y = ', listY)
+
+    sumXFirstDegree = aprx.calculateSumInAnyPower(listX, 1)
+    sumXSecondDegree = aprx.calculateSumInAnyPower(listX, 2)
+    sumXThirdDegree = aprx.calculateSumInAnyPower(listX, 3)
+    sumXFourthDegree = aprx.calculateSumInAnyPower(listX, 4)
+
+    normalSystemOfLeastSquares = aprx.mx.Matrix(3, 4, [sumXFourthDegree,
+                                                       sumXThirdDegree,
+                                                       sumXSecondDegree,
+                                                       aprx.calculteSumOfMultiplyElements(listX, 2, listY),
+                                                       sumXThirdDegree,
+                                                       sumXSecondDegree,
+                                                       sumXFirstDegree,
+                                                       aprx.calculteSumOfMultiplyElements(listX, 1, listY),
+                                                       sumXSecondDegree,
+                                                       sumXFirstDegree,
+                                                       len(listX),
+                                                       aprx.calculateSumInAnyPower(listY, 1)
+                                                       ])
+    linearPolynomial = aprx.createPolynomial(aprx.Gauss(normalSystemOfLeastSquares))
+
+    listXForPlot = np.linspace(listX[0], listX[len(listX) - 1], 1000)
+    listYForPlot = []
+    # красные точки
+    listgetYFromLinearPolynomial = []
+    for x in listX:
+        listgetYFromLinearPolynomial.append(linearPolynomial(x))
+
+    for x in listXForPlot:
+        listYForPlot.append(linearPolynomial(x))
+
+    print('Сумма ошибок ', aprx.calculateSumOfSquaredErrors(listY, listgetYFromLinearPolynomial))
+
+    fig = plt.subplots()
+    plt.scatter(listX, listY, c='r')
+    plt.plot(listXForPlot, listYForPlot, color="g")
+    plt.grid(True)
+    plt.show()
+
